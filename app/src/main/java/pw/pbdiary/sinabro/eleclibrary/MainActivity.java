aa06fe5a54ceec729b7692a7b5f93f72cbe58d40
윤정민(종이상자) <lego37yoon@outlook.com>
2020-03-14 16:33:13 +0900

    Initial commit

---
+++ app/src/main/java/pw/pbdiary/sinabro/eleclibrary/MainActivity.java
@@ -0,0 +1,14 @@
+package pw.pbdiary.sinabro.eleclibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
